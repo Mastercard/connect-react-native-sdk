5f1644b18b9eaba4bb996528546456bc6d9ba0f6
Prathamesh Salawkar <prathamesh.salawkar@mastercard.com>
2025-12-16 18:13:58 +0530

    Scrollview and refdirectUrl Fixes

---
+++ src/utils.ts
@@ -0,0 +1,20 @@
+import 'react-native-url-polyfill/auto';
+import { DEFAULT_REDIRECT_URL } from './constants';
+
+export const validateUrl = (url: string = '') => {
+  try {
+    if (!url) {
+      console.warn('Invalid URL: URL is empty/null/undefined');
+      return  DEFAULT_REDIRECT_URL;
+    }
+    const urlPattern = /^https:\/\//i;
+    if (!urlPattern.test(url)) {
+      console.warn('Invalid URL: Must start with https://');
+      return  DEFAULT_REDIRECT_URL;
+    }
+    return url;
+  } catch (error) {
+    console.warn('Error parsing URL:', error);
+    return  DEFAULT_REDIRECT_URL;
+  }
+};
