66fd25ad739379d9329e92e139bc3c8d2e6f66f1
Prathamesh Salawkar <prathamesh.salawkar@mastercard.com>
2026-01-09 19:48:20 +0530

    android build fix

--- ConnectReactNativeDemoApp/android/app/src/main/java/com/connectreactnativedemoapp/MainActivity.kt
+++ ConnectReactNativeDemoApp/android/app/src/main/java/com/connectreactnativedemoapp/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.connectreactnativedemoapp
+package com.mastercard.openbanking.connect.demo
 
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
--- ConnectReactNativeDemoApp/android/app/src/main/java/com/connectreactnativedemoapp/MainApplication.kt
+++ ConnectReactNativeDemoApp/android/app/src/main/java/com/connectreactnativedemoapp/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.connectreactnativedemoapp
+package com.mastercard.openbanking.connect.demo
 
 import android.app.Application
 import com.facebook.react.PackageList
